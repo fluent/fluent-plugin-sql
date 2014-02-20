0fcb9dd4a811135b37835182fcc77f9887537440
Kenichi Suzuki <suzuki778@gmail.com>
2014-02-20 23:38:47 +0900

    add limit condition

--- lib/fluent/plugin/in_sql.rb
+++ lib/fluent/plugin/in_sql.rb
@@ -95,7 +95,9 @@ module Fluent
         if last_record && last_update_value = last_record[@update_column]
           relation = relation.where("#{@update_column} > ?", last_update_value)
         end
-        relation = relation.order("#{@update_column} ASC").limit(limit)
+        if limit > 0
+          relation = relation.limit(limit)
+        end
 
         now = Engine.now
         entry_name = @model.table_name.singularize
