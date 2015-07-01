ca29746b364105f8938fb92a08151352105eb292
Hiroshi Hatake <hatake@clear-code.com>
2015-07-01 18:44:29 +0900

    Remove needless secret
    Because username does not conceal as usual.

--- lib/fluent/plugin/in_sql.rb
+++ lib/fluent/plugin/in_sql.rb
@@ -26,7 +26,7 @@ module Fluent
     config_param :port, :integer, :default => nil
     config_param :adapter, :string
     config_param :database, :string
-    config_param :username, :string, :default => nil, :secret => true
+    config_param :username, :string, :default => nil
     config_param :password, :string, :default => nil, :secret => true
     config_param :socket, :string, :default => nil
 
